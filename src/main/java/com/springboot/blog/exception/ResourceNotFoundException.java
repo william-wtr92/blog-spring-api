98480b1bdf84017ee8c8c36c1f9215098b49459d
william-wtr92 <wwautrin@hotmail.fr>
2024-01-03 20:37:02 +0100

    refactor: error handler corrections (add lombok and update state of variables)

--- src/main/java/com/springboot/blog/exception/ResourceNotFoundException.java
+++ src/main/java/com/springboot/blog/exception/ResourceNotFoundException.java
@@ -1,13 +1,15 @@
 package com.springboot.blog.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Getter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
-    private String resourceName;
-    private String fieldName;
-    private Long fieldValue;
+    private final String resourceName;
+    private final String fieldName;
+    private final Long fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
         super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
@@ -16,15 +18,4 @@ public class ResourceNotFoundException extends RuntimeException{
         this.fieldValue = fieldValue;
     }
 
-    public String getResourceName() {
-        return resourceName;
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public Long getFieldValue() {
-        return fieldValue;
-    }
 }

5c316c1aac69c8d203a3039c9be3ffea6b5e7096
william-wtr92 <wwautrin@hotmail.fr>
2024-01-04 22:33:53 +0100

    feat(post): add default error message on fields

--- src/main/java/com/springboot/blog/dto/PostDto.java
+++ src/main/java/com/springboot/blog/dto/PostDto.java
@@ -10,15 +10,15 @@ import java.util.Set;
 public class PostDto {
     private Long id;
 
-    @NotEmpty
+    @NotEmpty(message = "Title should not be null or empty")
     @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
 
-    @NotEmpty
-    @Size(min = 10, message = "Post title should have at least 10 characters")
+    @NotEmpty(message = "Description should not be null or empty")
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
 
-    @NotEmpty
+    @NotEmpty(message = "Content should not be null or empty")
     private String content;
     private Set<CommentDto> comments;
 }

aa2f9a4283734e37e3e12ce61801e9916992db8c
william-wtr92 <wwautrin@hotmail.fr>
2024-01-07 11:37:07 +0100

    feat(category): DTO & Repository creation

---
+++ src/main/java/com/springboot/blog/dto/CategoryDto.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.dto;
+
+import com.springboot.blog.entity.Post;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CategoryDto {
+    private Long id;
+
+    @NotEmpty(message = "Category name must be not empty or null")
+    @Size(min = 3, message = "Category name must have 3 characters minimum")
+    private String name;
+
+    @NotEmpty(message = "Category name must be not empty or null")
+    @Size(min = 10, message = "Category description must have 10 characters minimum")
+    private String description;
+
+    private List<Post> posts;
+}
